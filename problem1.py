e8d31192a0a09157a2c373a98d074a128f7ead54
Minh Le <minh.d.le27@gmail.com>
2018-04-18 10:54:16 -0700

    fixed p1 and added p2 method 2

--- problem1.py
+++ problem1.py
@@ -19,47 +19,44 @@ def mc_estimate(mu0,sigma0, n):
     
     return f_samples,weights
     
-
-#exp set up
-n_samples = 10**7
-n_points = 100
+n_samples = 10**8
 #problem 1
 mu = 2
 sigma = 1
 pi_function = np.vectorize(lambda x : norm.pdf(x,mu,sigma))
-l_n_samples = [i*n_samples//n_points for i in range(1,n_samples//n_points)]
 #theta1
 mu0 = 2
 sigma0 = 1
-f_samples,weights = mc_estimate(mu0, sigma0, n_samples)
-weighted_fsamples = f_samples*weights
-theta1 = [np.mean(weighted_fsamples[:n]) for n in l_n_samples]
-ess1 = [n/(1+np.var(weights[:n])) for n in l_n_samples]
-
-
+print("1")
+f_samples1,weights1 = mc_estimate(mu0, sigma0, n_samples)
+weighted_fsamples1 = f_samples1*weights1
 #theta2
 mu0 = 0
 sigma0 = 1
-f_samples,weights = mc_estimate(mu0, sigma0, n_samples)
-weighted_fsamples = f_samples*weights
-theta2 = [np.mean(weighted_fsamples[:n]) for n in l_n_samples]
-ess2 = [n/(1+np.var(weights[:n])) for n in l_n_samples]
-
+print("2")
+f_samples2,weights2 = mc_estimate(mu0, sigma0, n_samples)
+weighted_fsamples = f_samples2*weights2
 #theta3
 mu0 = 0
 sigma0 = 4
-f_samples,weights = mc_estimate(mu0, sigma0, n_samples)
-weighted_fsamples = f_samples*weights
-theta3 = [np.mean(weighted_fsamples[:n]) for n in l_n_samples]
-ess3 = [n/(1+np.var(weights[:n])) for n in l_n_samples]
+print('3')
+f_samples3,weights3 = mc_estimate(mu0, sigma0, n_samples)
+weighted_fsamples = f_samples3*weights3
 
+n_points = 100
+l_n_samples = [i*n_samples//n_points for i in range(1,n_points)]
 
-
+theta1 = [np.mean(weighted_fsamples1[:n]) for n in l_n_samples]
+ess1 = [n/(1+np.var(weights1[:n])) for n in l_n_samples]
+theta2 = [np.mean(weighted_fsamples2[:n]) for n in l_n_samples]
+ess2 = [n/(1+np.var(weights2[:n])) for n in l_n_samples]
+theta3 = [np.mean(weighted_fsamples3[:n]) for n in l_n_samples]
+ess3 = [n/(1+np.var(weights3[:n])) for n in l_n_samples]
 
 l_log_n_samples = np.log10(l_n_samples)
-plt.plot(l_log_n_samples, theta1,label="theta1")
-plt.plot(l_log_n_samples, theta2,label="theta2")
-plt.plot(l_log_n_samples, theta3,label="theta3")
+plt.plot(l_log_n_samples, np.log(theta1),label="theta1")
+plt.plot(l_log_n_samples, np.log(theta2),label="theta2")
+plt.plot(l_log_n_samples, np.log(theta3),label="theta3")
 plt.xlabel("number samples (log10n)")
 plt.ylabel("theta_hat")
 plt.legend()
--- problem2.py
+++ problem2.py
@@ -3,7 +3,7 @@ from enum import Enum
 import numpy as np
 import multiprocessing as mp
 from SAW import SAW
-import h5py
+import csv
 
 point = namedtuple('point', ['x','y'])
 
@@ -14,77 +14,76 @@ SAWSample = namedtuple('SAWSample', \
 #method 1 of computing px
 def draw_SAW_samples1(batch):
     saw = SAW(10)
-    px = 1
     attempts = 0
     samples = []
     fin_samples = []
-   
     for b in range(batch):
         saw.reset()
+        px = 1
         attempts += 1
         while saw.getNumValidMoves() > 0:
             px *= saw.getNumValidMoves()
             saw.randomMove()
             if saw.pos == point(10,10):
                 fin_samples.append(SAWSample(px/attempts, saw.length,saw.history))
+                attempts = 0
         samples.append(SAWSample(px,saw.length, saw.history))
     return samples, fin_samples
-#method 1 of computing px
-def draw_finished_SAW_sample1():
+#method 2 of computing px
+def draw_SAW_sample2():
     saw = SAW(10)
-    attempts = 1
-    while True:
-        px = 1
+    epsilon = 1e-6
+    attempts = 0
+    samples = []
+    fin_samples = []
+    for b in range(batch):
         saw.reset()
+        px = 1
+        attempts += 1
         while saw.getNumValidMoves() > 0:
             px *= saw.getNumValidMoves()
             saw.randomMove()
             if saw.pos == point(10,10):
-                return SAWSample(saw.length,px/attempts, saw.history)
-        attempts += 1
-    return None
-
-#method 2 of computing px
-
-
-def draw_SAW_sample2():
-    pass
-
+                fin_samples.append(SAWSample(px/attempts, saw.length,saw.history))
+                attempts = 0
+            if np.random.random() > epsilon:
+                break
+        samples.append(SAWSample(px,saw.length, saw.history))
+    return samples, fin_samples
 
 #experiment set up
-m = 10
-batch = 3
+m = 1000000
+batch =10000 
 draw_samples_fn = draw_SAW_samples1
-path_fname = "test.h5"
-fin_path_fname = "fin_test.h5"
+path_fname = "problem2/2.csv"
+fin_path_fname = "problem2/fin_2.csv"
 
 max_length = 0
 max_hist = None
 fin_max_length = 0
 fin_max_hist = None
 
-with h5py.File(path_fname, 'w') as f,h5py.File(fin_path_fname,'w') as fin_f:
-    dset = f.create_dataset('paths_info',(0,2),maxshape=(None,2),dtype='f',chunks=(batch,2))
-    fin_dset = fin_f.create_dataset('fin_paths_info',(0,2),maxshape=(None,2),dtype='f',chunks=(batch,2))
+n_fin_samples = 0
+with open(path_fname, 'w+') as f,open(fin_path_fname,'w+') as fin_f:
+    writer = csv.writer(f)
+    fin_writer = csv.writer(fin_f)
 
     while m > 0:
         samples,fin_samples = draw_samples_fn(batch=batch)
         n_samples = len(samples)
-        data = np.array([(sample.length,sample.px) for sample in samples]).astype(float)
+        data = np.array([(sample.px, sample.length) for sample in samples]).astype(float)
 
         batch_max_idx = np.argmax(data[:,1])
         if max_length < data[batch_max_idx,1]:
             max_length = data[batch_max_idx,1]
             max_hist = samples[batch_max_idx].history
-        dset.resize(dset.shape[0]+n_samples,axis=0) 
-        dset[-n_samples:] = data
-        
-        data = np.array([(sample.length,sample.px) for sample in fin_samples]).astype(float)
+        writer.writerows(data)
+
+        data = np.array([(sample.px, sample.length) for sample in fin_samples]).astype(float)
         if len(data) > 0:
             batch_max_idx = np.argmax(data[:,1])
             if fin_max_length < data[batch_max_idx,1]:
                 fin_max_length = data[batch_max_idx,1]
                 fin_max_hist = samples[batch_max_idx].history
-            fin_dset.resize(dset.shape[0]+n_samples,axis=0) 
-            fin_dset[-n_samples:] = data
             m -= n_samples
+        fin_writer.writerows(data)
