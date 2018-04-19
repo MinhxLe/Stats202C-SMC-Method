a7d886d09bdf1e8bb53d5d70d14c3616ac9ed6fa
Minh Le <minh.d.le27@gmail.com>
2018-04-19 12:32:15 -0700

    added function2

--- problem1.py
+++ problem1.py
@@ -1,4 +1,3 @@
-from scipy.stats import norm
 import numpy as np
 import matplotlib.pyplot as plt
 #Problem 1
@@ -7,13 +6,11 @@ def mc_estimate(mu0,sigma0, n):
     '''
     SMC method for predicting E[sqrt(x^2+y^2)] 
     '''
-    g_function = np.vectorize(lambda x : norm.pdf(x,mu0,sigma0))
-    
     samples = np.random.normal(loc=mu0, scale=sigma0, size=[n,2])
     pi_values = -1/(2*sigma**2) * np.square((samples-mu))
     g_values = -1/(2*sigma0**2) * np.square((samples-mu0))
-    pi_values = np.exp(pi_values)
-    g_values = np.exp(g_values)
+    pi_values = np.exp(pi_values)/sigma
+    g_values = np.exp(g_values)/sigma0
     pi_values = np.product(pi_values,axis=1)
     g_values = np.product(g_values,axis=1)
     f_samples = np.sqrt(np.sum(np.square(samples),axis=1))
@@ -21,7 +18,7 @@ def mc_estimate(mu0,sigma0, n):
     
     return f_samples,weights
     
-n_samples = 1000
+n_samples = 50000000
 #problem 1
 mu = 2
 sigma = 1
@@ -31,6 +28,8 @@ mu0 = 2
 sigma0 = 1
 print("1")
 f_samples1,weights1 = mc_estimate(mu0, sigma0, n_samples)
+#np.save("problem1/samples1.npy",f_samples1)
+#np.save("problem1/weights1.npy",weights1)
 weighted_fsamples1 = f_samples1*weights1
 #theta2
 mu0 = 0
@@ -45,9 +44,12 @@ print('3')
 f_samples3,weights3 = mc_estimate(mu0, sigma0, n_samples)
 weighted_fsamples3 = f_samples3*weights3
 
-n_points = 100
-l_n_samples = [i*n_samples//n_points for i in range(1,n_points)]
-
+# n_points = 100
+# l_n_samples = [i*n_samples//n_points for i in range(1,n_points)]
+# if l_n_samples[0] > 100:
+    # l_n_samples = [10,100,1000] + l_n_samples
+l_n_samples = [10,100,1000,10000,100000,1000000,\
+        10000000,25000000,50000000]
 theta1 = [np.mean(weighted_fsamples1[:n]) for n in l_n_samples]
 ess1 = [n/(1+np.var(weights1[:n])) for n in l_n_samples]
 theta2 = [np.mean(weighted_fsamples2[:n]) for n in l_n_samples]
@@ -56,17 +58,21 @@ theta3 = [np.mean(weighted_fsamples3[:n]) for n in l_n_samples]
 ess3 = [n/(1+np.var(weights3[:n])) for n in l_n_samples]
 
 l_log_n_samples = np.log10(l_n_samples)
-plt.plot(l_log_n_samples, np.log(theta1),label="theta1")
-plt.plot(l_log_n_samples, np.log(theta2),label="theta2")
-plt.plot(l_log_n_samples, np.log(theta3),label="theta3")
+plt.plot(l_log_n_samples, theta1,label="theta1")
+plt.plot(l_log_n_samples, theta2,label="theta2")
+plt.plot(l_log_n_samples, theta3,label="theta3")
 plt.xlabel("number samples (log10n)")
 plt.ylabel("theta_hat")
 plt.legend()
+plt.show()
 
 
-
+#10^3
+#10^7
 #looking at graph, we see 
-
-# plt.xlabel("number samples (log10n)")
-# plt.ylabel("ess(n)/ess*(n)")
-# plt.legend()
+plt.plot(l_log_n_samples,ess2/ess2[2])
+plt.plot(l_log_n_samples,ess3/ess3[5])
+plt.xlabel("number samples (log10n)")
+plt.ylabel("ess(n)/ess*(n)")
+plt.show()
+plt.legend()
--- problem2.py
+++ problem2.py
@@ -6,7 +6,7 @@ from SAW import SAW
 import csv
 import pickle
 import argparse
-
+import copy 
 point = namedtuple('point', ['x','y'])
 
 
@@ -14,7 +14,7 @@ SAWSample = namedtuple('SAWSample', \
         ['px','length','history'])
 
 #method 1 of computing px
-def draw_SAW_samples1(batch):
+def draw_SAW_samples0(batch):
     saw = SAW(10)
     attempts = 0
     samples = []
@@ -32,7 +32,7 @@ def draw_SAW_samples1(batch):
         samples.append(SAWSample(px,saw.length, saw.history))
     return samples, fin_samples
 #method 2 of computing px (epsilon)
-def draw_SAW_samples2(batch):
+def draw_SAW_samples1(batch):
     saw = SAW(10)
     epsilon = 1e-8
     attempts = 0
@@ -52,10 +52,49 @@ def draw_SAW_samples2(batch):
                 attempts = 0
         samples.append(SAWSample(px,saw.length, saw.history))
     return samples, fin_samples
-def draw_SAW_samples3(batch):
-    pass
+def continue_SAW_sample2(saw,px,attempts,split):
+    saw = copy.deepcopy(saw) 
+    num_moves = saw.getNumValidMoves()
+    fin_sample = None
+    while num_moves > 0:
+        px *= num_moves
+        saw.randomMove()
+        if saw.pos == point(10,10):
+            fin_sample = SAWSample(px/split,saw.length,saw.history)
+        num_moves = saw.getNumValidMoves()
+    sample = SAWSample(px/split,saw.length,saw.history)
+    return sample,fin_sample
+def draw_SAW_samples2(batch):
+    saw = SAW(10)
+    attempts = 0
+    samples = []
+    fin_samples = []
+    do_branch = False
+    n_split = 5
+    for b in range(batch):
+        saw.reset()
+        px = 1
+        attempts += 1
+        while saw.getNumValidMoves() > 0:
+            px *= saw.getNumValidMoves()
+            saw.randomMove()
+            if saw.pos == point(10,10):
+                fin_samples.append(SAWSample(px/attempts, saw.length,saw.history))
+                attempts = 0
+            if saw.length >= 50:
+                do_branch = True
+                for i in range(0,n_split):
+                    sample,fin_sample = continue_SAW_sample2(saw,px,attempts,n_split)
+                    if fin_sample:
+                        fin_samples.append(fin_sample)
+                    samples.append(sample)
+                break
+        if not do_branch:
+            samples.append(SAWSample(px,saw.length, saw.history))
+        do_branch = False
+    return samples, fin_samples
 
-draw_samples_fns = [draw_SAW_samples1,draw_SAW_samples2, draw_SAW_samples3]
+draw_samples_fns = [draw_SAW_samples0,draw_SAW_samples1, draw_SAW_samples2]
 
 #argument parsing
 parser = argparse.ArgumentParser()
@@ -80,11 +119,11 @@ fin_max_length = 0
 fin_max_hist = None
 
 n_fin_samples = 0 
-with open(path_fname, 'w+') as f,open(fin_path_fname,'w+') as fin_f:
+with open(path_fname, 'a+') as f,open(fin_path_fname,'a+') as fin_f:
     writer = csv.writer(f)
     fin_writer = csv.writer(fin_f)
-
-    while m > 0:
+    counter = m
+    while counter > 0:
         samples,fin_samples = draw_samples_fn(batch=batch)
         n_samples = len(samples)
         data = np.array([(sample.px, sample.length) for sample in samples]).astype(float)
@@ -94,16 +133,16 @@ with open(path_fname, 'w+') as f,open(fin_path_fname,'w+') as fin_f:
             max_length = data[batch_max_idx,1]
             max_hist = samples[batch_max_idx].history
         writer.writerows(data)
-
         data = np.array([(sample.px, sample.length) for sample in fin_samples]).astype(float)
         if len(data) > 0:
             batch_max_idx = np.argmax(data[:,1])
             if fin_max_length < data[batch_max_idx,1]:
                 fin_max_length = data[batch_max_idx,1]
                 fin_max_hist = samples[batch_max_idx].history
-            m -= n_samples
-        fin_writer.writerows(data)
-
+            counter -= n_samples
+            fin_writer.writerows(data)
+            n_fin_samples += len(data)
+            print(n_fin_samples)
 #save max history with pickle
 with open('problem2/max_length_{}.pkl'.format(fn_choice),'wb') as f:
     pickle.dump(max_hist,f)
