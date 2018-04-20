cdf6da17b99deb271f92d01b36b102e7699d4c3c
Minh Le <minh.le@ucla.edu>
2018-04-20 15:29:24 -0700

    idk

--- problem2.py
+++ problem2.py
@@ -20,16 +20,16 @@ def draw_SAW_samples0(batch):
     samples = []
     fin_samples = []
     for b in range(batch):
-        px = []
+        px = 1
         saw.reset()
         attempts += 1
         while saw.getNumValidMoves() > 0:
-            px.append(saw.getNumValidMoves())
+            px *= saw.getNumValidMoves()
             saw.randomMove()
         if saw.pos == point(10,10):
-            fin_samples.append(SAWSample(np.prod(px)/attempts, saw.length,saw.history))
+            fin_samples.append(SAWSample(px/attempts, saw.length,copy.copy(saw.history)))
             attempts = 0
-        samples.append(SAWSample(np.prod(px),saw.length, saw.history))
+        samples.append(SAWSample(px,saw.length, copy.copy(saw.history)))
     return samples, fin_samples
 #method 2 of computing px (epsilon)
 def draw_SAW_samples1(batch):
@@ -45,14 +45,38 @@ def draw_SAW_samples1(batch):
         while saw.getNumValidMoves() > 0:
             if np.random.random() < epsilon:
                 break
-            px *= saw.getNumValidMoves()
+            px *= saw.getNumValidMoves()/(1-epsilon)
             saw.randomMove()
         if saw.pos == point(10,10):
-            fin_samples.append(SAWSample(np.prod(px)/attempts, saw.length,saw.history))
+            fin_samples.append(SAWSample(np.prod(px)/attempts, saw.length,copy.copy(saw.history)))
             attempts = 0
-        samples.append(SAWSample(np.prod(px),saw.length, saw.history))
+        samples.append(SAWSample(np.prod(px),saw.length, copy.copy(saw.history)))
     return samples, fin_samples
-def continue_SAW_sample2(saw,px,attempts,split):
+
+#method 3 of computing px (epsilon)
+def draw_SAW_samples2(batch):
+    saw = SAW(10)
+    epsilon = 1e-8
+    attempts = 0
+    samples = []
+    fin_samples = []
+    for b in range(batch):
+        saw.reset()
+        px = 1
+        attempts += 1
+        epsilon=1e-8
+        while saw.getNumValidMoves() > 0:
+            if np.random.random() < epsilon:
+                break
+            epsilon *= .5
+            px *= saw.getNumValidMoves()/(1-epsilon)
+            saw.randomMove()
+        if saw.pos == point(10,10):
+            fin_samples.append(SAWSample(np.prod(px)/attempts, saw.length,copy.copy(saw.history)))
+            attempts = 0
+        samples.append(SAWSample(np.prod(px),saw.length, copy.copy(saw.history)))
+    return samples, fin_samples
+def continue_SAW_sample3(saw,px,attempts,split):
     saw = copy.deepcopy(saw) 
     num_moves = saw.getNumValidMoves()
     fin_sample = None
@@ -64,7 +88,7 @@ def continue_SAW_sample2(saw,px,attempts,split):
         num_moves = saw.getNumValidMoves()
     sample = SAWSample(px/split,saw.length,saw.history)
     return sample,fin_sample
-def draw_SAW_samples2(batch):
+def draw_SAW_samples3(batch):
     saw = SAW(10)
     attempts = 0
     samples = []
@@ -84,7 +108,7 @@ def draw_SAW_samples2(batch):
             if saw.length >= 50:
                 do_branch = True
                 for i in range(0,n_split):
-                    sample,fin_sample = continue_SAW_sample2(saw,px,attempts,n_split)
+                    sample,fin_sample = continue_SAW_sample3(saw,px,attempts,n_split)
                     did_finish = False
                     if fin_sample:
                         fin_samples.append(fin_sample)
@@ -108,7 +132,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--samples', help='number of samples', type=int)
 parser.add_argument('--batch', help='number of samples', type=int)
 parser.add_argument('--fn', help='number of samples', type=int)
-parser.add_argument('--seed',type=int)
+parser.add_argument('--seed',help='random see',type=int)
 #TODO parameter checking
 
 #seed
@@ -128,16 +152,15 @@ max_hist = None
 fin_max_length = 0
 fin_max_hist = None
 
-n_fin_samples = 0 
 with open(path_fname, 'w+') as f,open(fin_path_fname,'w+') as fin_f:
     writer = csv.writer(f)
     fin_writer = csv.writer(fin_f)
     counter = m
     while counter > 0:
+        print(counter)
         samples,fin_samples = draw_samples_fn(batch=batch)
-        n_samples = len(samples)
         data = np.array([(sample.px, sample.length) for sample in samples]).astype(float)
-
+        counter -= len(data)
         batch_max_idx = np.argmax(data[:,1])
         if max_length < data[batch_max_idx,1]:
             max_length = data[batch_max_idx,1]
@@ -149,12 +172,13 @@ with open(path_fname, 'w+') as f,open(fin_path_fname,'w+') as fin_f:
             if fin_max_length < data[batch_max_idx,1]:
                 fin_max_length = data[batch_max_idx,1]
                 fin_max_hist = samples[batch_max_idx].history
-            counter -= n_samples
             fin_writer.writerows(data)
-            n_fin_samples += len(data)
-            print(n_fin_samples)
+    f.flush()
+    fin_f.flush()
 #save max history with pickle
-with open('problem2/fn{}/max_length{}.pkl'.format(fn_choice,args.seed),'wb') as f:
-    pickle.dump(max_hist,f)
-with open('problem2/fin_fn{}/max_length{}.pkl'.format(fn_choice,args.seed),'wb') as f:
-    pickle.dump(fin_max_hist,f)
+if max_hist:
+    with open('problem2/fn{}/max_length{}.pkl'.format(fn_choice,args.seed),'wb') as f:
+        pickle.dump(max_hist,f)
+if fin_max_hist:
+    with open('problem2/fin_fn{}/max_length{}.pkl'.format(fn_choice,args.seed),'wb') as f:
+        pickle.dump(fin_max_hist,f)
---
+++ problem2_analysis.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import argparse
+import os
+import glob
+parser = argparse.ArgumentParser()
+parser.add_argument('--fn', help='number of samples', type=int)
+parser.add_argument('--fin', help='n to n',type=bool)
+args = parser.parse_args()
+
+if args.fin:
+    fin_str = "fin_"
+else:
+    fin_str = ""
+data = None
+checked = False
+for fname in glob.glob("./problem2/{}fn{}/*.csv".format(fin_str,args.fn)):
+    if os.stat(fname).st_size == 0:
+        continue
+    fdata = pd.read_csv(fname).values
+    if not checked and  data == None:
+        data = fdata
+        checked = True
+    else:
+        data = np.append(data,fdata,axis=0)
+print(len(data))
+
+
